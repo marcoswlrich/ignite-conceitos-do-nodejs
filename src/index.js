26289ff290309197f187c970d414630000e91b53
marcoswlrich <marcosapw.dev@gmail.com>
2022-02-18 01:56:37 -0300

    Funçao de verificaçao e rota de retorno de todo

--- src/index.js
+++ src/index.js
@@ -10,7 +10,20 @@ app.use(express.json());
 
 const users = [];
 
-function checksExistsUserAccount(request, response, next) {}
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response
+      .status(400)
+      .json({ error: "This user account does not exist" });
+  }
+
+  request.user = user;
+
+  return next();
+}
 
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
@@ -28,7 +41,9 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
